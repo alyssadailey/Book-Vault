899723df0ada531a9d3d2122cb65dedddab63c63
alyssadailey <alyssadailey28@gmail.com>
2025-04-14 15:13:27 -0400

    updated file paths in server.ts to render correct file paths

--- Develop/server/src/server.ts
+++ Develop/server/src/server.ts
@@ -66,9 +66,9 @@ async function startServer() {
 
   // Serve static files if in production
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/dist')));
+    app.use(express.static(path.join(__dirname, '../../client/dist')));
     app.get('*', (_req, res) =>
-      res.sendFile(path.join(__dirname, '../client/dist/index.html'))
+      res.sendFile(path.join(__dirname, '../../client/dist/index.html'))
     );
   }
 
